3d96b652f4fc41e51f398fe58819172a4b9284cd
h4mxa <duke80800@gmail.com>
2023-08-27 23:39:37 +0500

    getUrlsFromHTML function and tests

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require("./crawl.js");
+const { normalizeURL, getURLsFromHTML } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL strip protocol", () => {
@@ -26,5 +26,58 @@ test("normalizeURL strip http", () => {
   const input = "http://example.com/path";
   const actual = normalizeURL(input);
   const expected = "example.com/path";
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML absolute", () => {
+  const inputHTMLBody = `
+  <html>
+    <body>
+      <a href="http://example.com/path/"></a>
+    </body>
+  </html>`;
+  const inputBaseURL = "http://example.com/path/";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["http://example.com/path/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML relative", () => {
+  const inputHTMLBody = `
+  <html>
+    <body>
+      <a href="/path/"></a>
+    </body>
+  </html>`;
+  const inputBaseURL = "http://example.com";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["http://example.com/path/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML both", () => {
+  const inputHTMLBody = `
+  <html>
+    <body>
+      <a href="http://example.com/path1/">Example path 1</a>
+      <a href="/path2/">Example path 2</a>
+    </body>
+  </html>`;
+  const inputBaseURL = "http://example.com";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["http://example.com/path1/", "http://example.com/path2/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML invalid", () => {
+  const inputHTMLBody = `
+  <html>
+    <body>
+      <a href="invalid">Invalid</a>
+    </body>
+  </html>`;
+  const inputBaseURL = "http://example.com";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [];
   expect(actual).toEqual(expected);
 });
